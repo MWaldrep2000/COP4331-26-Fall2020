f41e998e8e0157e81fa364523995ad24f5a4295d
JoelMarq <joel.marquez@knights.ucf.edu>
2020-09-03 23:32:45 -0400

    Created and modified Login.php file, should be complete.

--- LAMPAPI/Login.php
+++ LAMPAPI/Login.php
@@ -1,36 +1,59 @@
 <?php
 
+	// All variables in php have $ (dollar signs) in front of them
+	// All objects in php have $ (dollar signs) in front of them
+
+
 	$inData = getRequestInfo();
 	
+	// Creating variables for use later
 	$id = 0;
 	$firstName = "";
 	$lastName = "";
 
-	$conn = new mysqli("localhost", "ricklein_leinecker", "WeLoveCOP4331", "ricklein_COP4331");
+	// Creates connection between php and database
+	$conn = new mysqli("localhost", "mwaldrep_mwaldrep", "Seagull123$", "mwaldrep_contactyContacts");
+
+	// Checking the connect_error variable in the $conn object
 	if ($conn->connect_error) 
 	{
 		returnWithError( $conn->connect_error );
 	} 
+
 	else
 	{
-		$sql = "SELECT ID,firstName,lastName FROM Users where Login='" . $inData["login"] . "' and Password='" . $inData["password"] . "'";
+		// dots below mean string concatenation, SELECT FROM are keywords in sql
+		// This code is creating the query request to get data from the database
+		$sql = "SELECT ID,FirstName,LastName FROM Users where Login='" . $inData["login"] . "' and Password='" . $inData["password"] . "'";
+
+		// Performs query on sql database, this uses the function query() from the $conn object
 		$result = $conn->query($sql);
+
+		// If there is data
 		if ($result->num_rows > 0)
 		{
+			// Gets the information from the result object using the fetch_assoc() function in the
+			// result object and stores it in the row object
 			$row = $result->fetch_assoc();
-			$firstName = $row["firstName"];
-			$lastName = $row["lastName"];
+			$firstName = $row["FirstName"];
+			$lastName = $row["LastName"];
 			$id = $row["ID"];
 			
+			// returns the following objects as a json file
 			returnWithInfo($firstName, $lastName, $id );
 		}
+
+		// If there is no data
 		else
 		{
 			returnWithError( "No Records Found" );
 		}
+
+		// Closes the connection to the database
 		$conn->close();
 	}
 	
+	// Takes in json object and outputs a php object
 	function getRequestInfo()
 	{
 		return json_decode(file_get_contents('php://input'), true);
@@ -39,9 +62,12 @@
 	function sendResultInfoAsJson( $obj )
 	{
 		header('Content-type: application/json');
+
+		// Prints out the json object
 		echo $obj;
 	}
 	
+	// Creates and returns a json object with values that show an error
 	function returnWithError( $err )
 	{
 		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';

4176ec90eaa65d36bf46823c6e6cbddbfb34408d
JoelMarq <joel.marquez@knights.ucf.edu>
2020-09-12 17:29:01 -0400

    Added comments and changed server connection.

--- LAMPAPI/Search.php
+++ LAMPAPI/Search.php
@@ -1,19 +1,32 @@
 <?php
 
+    // CURRENT QUESTIONS
+    // How exactly did the original SearchColors.php file function?
+    // What does the blocked off code do?
+    // How do we know if the website is doing a get or a post request?
+    // How are we formatting the output from the search query?
+
 	$inData = getRequestInfo();
 	
 	$searchResults = "";
 	$searchCount = 0;
 
+    // Correct connection
 	$conn = new mysqli("localhost:3306", "mwaldrep_spade", "Seagull123$", "mwaldrep_contactyContacts");
 	if ($conn->connect_error) 
 	{
 		returnWithError( $conn->connect_error );
-	} 
+    } 
+    
+
 	else
 	{
-		$sql = "select Name from Colors where Name like '%" . $inData["search"] . "%' and UserID=" . $inData["userId"];
-		$result = $conn->query($sql);
+        $sql = "select Name from Colors where Name like '%" . $inData["search"] . "%' and UserID=" . $inData["userId"];
+        
+        // This runs the query and returns data into the $result object
+        $result = $conn->query($sql);
+        
+        /////////////////////////////////////////////////////////////////////////////////////////////////
 		if ($result->num_rows > 0)
 		{
 			while($row = $result->fetch_assoc())
@@ -21,15 +34,19 @@
 				if( $searchCount > 0 )
 				{
 					$searchResults .= ",";
-				}
+                }
+                
 				$searchCount++;
 				$searchResults .= '"' . $row["Name"] . '"';
 			}
-		}
+        }
+        /////////////////////////////////////////////////////////////////////////////////////////////////
+
 		else
 		{
 			returnWithError( "No Records Found" );
-		}
+        }
+        
 		$conn->close();
 	}
 

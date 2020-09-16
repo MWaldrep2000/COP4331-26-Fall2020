df2b63952b2720dd2fc8f380899c4c778ce79d89
gsergio96 <70297217+gsergio96@users.noreply.github.com>
2020-09-16 11:52:52 -0400

    Update code.js

--- js/code.js
+++ js/code.js
@@ -1,175 +1,191 @@
-var urlBase = 'http://COP4331-3.com/LAMPAPI';
+var urlBase = 'http://COP4331-26.com/LAMPAPI';
 var extension = 'php';
 
 var userId = 0;
 var firstName = "";
 var lastName = "";
+var Address = "";
+var PhoneNumber = "";
+var Email = "";
+var ContactID = "";
 
-function doLogin()
+// ******** DELETE LATER ******************
+function FakeLogin()
+{
+	var narutoNames = ["Naruto Uzumaki", 
+						"Sasuke Uchiha", 
+						"Sakura Haruno", 
+						"Rock Lee", 
+						"Neji Hyuga", 
+						"Tenten IDK", 
+						"Hinata Hyuga", 
+						"Shino Aburame", 
+						"Kiba Inuzuka", 
+						"Ino Yamanaka", 
+						"Shikamaru Nara", 
+						"Choji Akamichi"
+						];
+
+	var select = document.getElementById("select");
+
+	for(var i = 0; i < narutoNames.length; i++){
+		var name = narutoNames[i];
+		var element = document.createElement("option");
+		element.value = name;
+		element.textContent = name;
+		select.appendChild(element);
+	}
+					
+}
+// *********************************************************
+
+function AddContact()
+{
+    firstName = document.getElementById("firstName").value;
+    lastName = document.getElementById("lastName").value;
+    PhoneNumber= document.getElementById("PhoneNumber").value;
+    Email = document.getElementById("Email").value;
+    Address = document.getElementById("Address").value;
+    
+    document.getElementbyId("ContactAddRessult").innerHTML = "";
+    
+    var myObject = { firstName: firstName, lastName: lastName, PhoneNumber: PhoneNumber, Email: Email, Address: Address};
+    var jsonPayload = JSON.stringify(myObject);
+    alert(jsonPayload);
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+    
+    try
+    {
+        xhr.onreadstatechange = function()
+        {
+            if(this.readySate == 4 && this.status == 200)
+            {
+                document.getElementById("ContactAddResult").innerHTML = "Contact has been added";
+            }
+        };
+        xhr.send(jsonPayload);
+    }
+    catch(err)
+    {
+        document.getElementbyId("ContactAddResult").innerHTML = err.message;
+    }
+}
+
+function Login()
 {
 	userId = 0;
 	firstName = "";
 	lastName = "";
 	
-	var login = document.getElementById("loginName").value;
-	var password = document.getElementById("loginPassword").value;
-//	var hash = md5( password );
+	var base_url = window.location.origin;
 	
-	document.getElementById("loginResult").innerHTML = "";
+	var login = document.getElementById("username").value;
 
-//	var jsonPayload = '{"login" : "' + login + '", "password" : "' + hash + '"}';
-	var jsonPayload = '{"login" : "' + login + '", "password" : "' + password + '"}';
+	var password = document.getElementById("password").value;
+	var hash = md5( password );
+
+//	var jsonPayload = '{"login" : "' + login + '", "password" : "' + password + '"}';
+	
+	var myObject = { login: login, password : password}; //this maybe slightly easier
+	var jsonPayload = JSON.stringify(myObject);     //this maybe slightly easier
+	
+	alert(jsonPayload)
+	
 	var url = urlBase + '/Login.' + extension;
-
+	alert(url);
+	
 	var xhr = new XMLHttpRequest();
-	xhr.open("POST", url, false);
-	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	xhr.open("POST", url, false); //opening connection telling its gonna be a post, false means synchronous means less code
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8"); //telling it im talking in json
 	try
 	{
 		xhr.send(jsonPayload);
+		alert(jsonPayload);
 		
 		var jsonObject = JSON.parse( xhr.responseText );
+		//or jsonObject["id"]
 		
 		userId = jsonObject.id;
-		
+		alert(userId);
 		if( userId < 1 )
 		{
 			document.getElementById("loginResult").innerHTML = "User/Password combination incorrect";
 			return;
 		}
-		
 		firstName = jsonObject.firstName;
 		lastName = jsonObject.lastName;
-
-		saveCookie();
+		alert(firstName);
+		alert(lastName);
+		document.getElementById("loginResult").innerHTML = "Login success";
 	
-		window.location.href = "color.html";
+		window.location.href = "userpage.html"; //edit for next page
 	}
 	catch(err)
 	{
 		document.getElementById("loginResult").innerHTML = err.message;
 	}
 
+// WHEN THEY LOGIN WE NEED TO HAVE SOMETHING THAT GETS ALL OF THE CONTACTS
 }
 
-function saveCookie()
+function Signup()
 {
-	var minutes = 20;
-	var date = new Date();
-	date.setTime(date.getTime()+(minutes*60*1000));	
-	document.cookie = "firstName=" + firstName + ",lastName=" + lastName + ",userId=" + userId + ";expires=" + date.toGMTString();
-}
-
-function readCookie()
-{
-	userId = -1;
-	var data = document.cookie;
-	var splits = data.split(",");
-	for(var i = 0; i < splits.length; i++) 
-	{
-		var thisOne = splits[i].trim();
-		var tokens = thisOne.split("=");
-		if( tokens[0] == "firstName" )
-		{
-			firstName = tokens[1];
-		}
-		else if( tokens[0] == "lastName" )
-		{
-			lastName = tokens[1];
-		}
-		else if( tokens[0] == "userId" )
-		{
-			userId = parseInt( tokens[1].trim() );
-		}
-	}
-	
-	if( userId < 0 )
-	{
-		window.location.href = "index.html";
-	}
-	else
-	{
-		document.getElementById("userName").innerHTML = "Logged in as " + firstName + " " + lastName;
-	}
-}
-
-function doLogout()
-{
-	userId = 0;
+// gets all the values from the text boxes
+    userId = 0;
 	firstName = "";
 	lastName = "";
-	document.cookie = "firstName= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
-	window.location.href = "index.html";
-}
+    
+    firstName = document.getElementById("firstName").value;
+    lastName = document.getElementById("lastName").value;
+    
+    var login = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    var confirmpassword = document.getElementById("confirmpassword").value;
+    
+// checks if the passwords are the same    
+    if(password != confirmpassword)
+    {
+        document.getElementById("RegisterResult").innerHTML = "Passwords do not match";
+        return;
+    }
 
-function addColor()
-{
-	var newColor = document.getElementById("colorText").value;
-	document.getElementById("colorAddResult").innerHTML = "";
+// create an object that contains all of the values from
+// the user inputs. then converts them to a json payload
+    var myObject = { firstName: firstName, lastName:lastName, login: login, password : password}; //this maybe slightly easier
+    var jsonPayload = JSON.stringify(myObject);
+    
+    var url = urlBase + '/Register.' + extension;
+    
+    var xhr = new XMLHttpRequest();
+	xhr.open("POST", url, false); //opening connection telling its gonna be a post, false means synchronous means less code
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8"); //telling it im talking in json
 	
-	var jsonPayload = '{"color" : "' + newColor + '", "userId" : ' + userId + '}';
-	var url = urlBase + '/AddColor.' + extension;
-	
-	var xhr = new XMLHttpRequest();
-	xhr.open("POST", url, true);
-	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
 	try
 	{
-		xhr.onreadystatechange = function() 
-		{
-			if (this.readyState == 4 && this.status == 200) 
-			{
-				document.getElementById("colorAddResult").innerHTML = "Color has been added";
-			}
-		};
 		xhr.send(jsonPayload);
+		alert(jsonPayload);
+		
+		var jsonObject = JSON.parse( xhr.responseText );
+		//or jsonObject["id"]
+		
+		userId = jsonObject.id;
+		alert(userId);
+		if( userId < 1 )
+		{
+			document.getElementById("RegisterResult").innerHTML = "Username has already been taken";
+			return;
+		}
+		document.getElementById("RegisterResult").innerHTML = "Registration successful";
+
+		//saveCookie();             //decide if we wanna save cookies
+	
+		window.location.href = "index.html"; //edit for next page
 	}
 	catch(err)
 	{
-		document.getElementById("colorAddResult").innerHTML = err.message;
+		document.getElementById("RegisterResult").innerHTML = err.message;
 	}
-	
-}
 
-function searchColor()
-{
-	var srch = document.getElementById("searchText").value;
-	document.getElementById("colorSearchResult").innerHTML = "";
-	
-	var colorList = "";
-	
-	var jsonPayload = '{"search" : "' + srch + '","userId" : ' + userId + '}';
-	var url = urlBase + '/SearchColors.' + extension;
-	
-	var xhr = new XMLHttpRequest();
-	xhr.open("POST", url, true);
-	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-	try
-	{
-		xhr.onreadystatechange = function() 
-		{
-			if (this.readyState == 4 && this.status == 200) 
-			{
-				document.getElementById("colorSearchResult").innerHTML = "Color(s) has been retrieved";
-				var jsonObject = JSON.parse( xhr.responseText );
-				
-				for( var i=0; i<jsonObject.results.length; i++ )
-				{
-					colorList += jsonObject.results[i];
-					if( i < jsonObject.results.length - 1 )
-					{
-						colorList += "<br />\r\n";
-					}
-				}
-				
-				document.getElementsByTagName("p")[0].innerHTML = colorList;
-			}
-		};
-		xhr.send(jsonPayload);
-	}
-	catch(err)
-	{
-		document.getElementById("colorSearchResult").innerHTML = err.message;
-	}
-	
 }
